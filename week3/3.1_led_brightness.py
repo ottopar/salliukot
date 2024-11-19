7363c37410ae6bc3f19e6b243192abfd5de6a28c
Niko Mehiläinen <156897404+mehiis@users.noreply.github.com>
2024-11-19 11:19:40 +0200

    Update 3.1_led_brightness.py
    added more comments

--- week3/3.1_led_brightness.py
+++ week3/3.1_led_brightness.py
@@ -25,19 +25,20 @@ class RotaryKnob:
         #CANCEL BUTTON BOUNCE
         self.can_press:bool = True
         self.last_press = 0
-        
+
+    #ENCODER BUTTON PRESS INTERUPT (ON RELEASE)
     def on_rotary_pressed(self, pin):
          if self.sw():
-                self.fifo.put(2)
-  
+                self.fifo.put(2) #2 = ROTARY PRESS
+    #ENCODER ROTATION (CHECK ONLY INNER RING)
     def on_rotary_rotated(self, pin):
         if self.b():
             self.fifo.put(-1)
         else:
             self.fifo.put(1)
-
+    #MAIN CODE
     def execute(self):
-        if self.fifo.has_data():
+        if self.fifo.has_data(): #IF self.fifo has any values
             value = self.fifo.get()
             
             if(value == 2 and self.can_press):
@@ -51,6 +52,7 @@ class RotaryKnob:
                 else:
                     self.is_led_on = True
                     print("Led on.")
+            
             elif(self.is_led_on):
                 self.brightness += value * 1500
                 
@@ -58,19 +60,19 @@ class RotaryKnob:
         self.brightness = value
         self.led_pwm.duty_u16(value)
         self.bounce_filter()
-    
+    #CLAMP FUNCTION (CLAMP GIVEN VALUE BETWEEN TWO GIVEN VALUE)
     def clamp(self, value, minimum, maximum):
         if(value > maximum):
             value = maximum
         elif(value < minimum):
             value = minimum
         return value
-
+    #ADD DELAY BETWEEN ENCODERS BUTTON PRESS
     def bounce_filter(self):
         if(time.ticks_ms() - self.last_press > 250 and not self.can_press):
             self.can_press = True
-    
+#ASSIGN ONE ROTARY    
 knob1 = RotaryKnob(12, 10, 11, 22, 1000) #(ROTARY_BTN_PIN, OUTER_ENCODER, INNER_ENCODER, FREQUENCY)
-
+#MAIN LOOP
 while True:
     knob1.execute() #RUN CODE
