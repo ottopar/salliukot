809a46b0d73b05fe8e9e2514609b9a8941c043fd
Niko Mehiläinen <156897404+mehiis@users.noreply.github.com>
2024-11-19 09:36:37 +0200

    Add files via upload

---
+++ week3/3.2_led_brightness.py
@@ -0,0 +1,77 @@
+#LIBRARYS
+from machine import Pin, PWM
+from fifo import Fifo
+import time
+from piotimer import Piotimer
+
+class RotaryKnob:
+    def __init__(self, rotarybtn_pin, a_pin, b_pin, led_pin, frequency):
+        self.sw = Pin(rotarybtn_pin, mode = Pin.IN, pull = Pin.PULL_UP)
+        self.a = Pin(a_pin, mode = Pin.IN, pull = Pin.PULL_UP)
+        self.b = Pin(b_pin, mode = Pin.IN, pull = Pin.PULL_UP)
+        
+        self.led_pwm = PWM(Pin(led_pin, Pin.OUT))
+        self.led_pwm.freq(frequency)
+        
+        self.brightness = 0
+        self.is_led_on:bool = True
+
+        self.fifo = Fifo(100, typecode = 'i')
+        self.a.irq(handler = self.on_rotary_rotated, trigger = Pin.IRQ_RISING, hard = True)
+        self.sw.irq(handler = self.on_rotary_pressed, trigger = Pin.IRQ_RISING, hard = True)
+        
+        #CANCEL BUTTON BOUNCE
+        self.can_press:bool = True
+        self.last_press = 0
+        
+    def on_rotary_pressed(self, pin):
+         if self.sw():
+                self.fifo.put(2)
+  
+    def on_rotary_rotated(self, pin):
+        if self.b():
+            self.fifo.put(-1)
+        else:
+            self.fifo.put(1)
+
+    def execute(self):
+        if self.fifo.has_data():
+            value = self.fifo.get()
+            print(value)
+            
+            if(value == 2 and self.can_press):
+                self.brightness = 0 #turn off the light what ever happens
+                self.last_press = time.ticks_ms() #this sets the last pressed tick to current time tick in milliseconds to prevent button bounce
+                self.can_press = False
+                
+                if(self.is_led_on):
+                    self.is_led_on = False
+                    print("Led off.")
+                else:
+                    self.is_led_on = True
+                    print("Led on.")
+            elif(self.is_led_on):
+                self.brightness += value * 1500
+                
+        value = self.clamp(self.brightness, 0, 65535)
+        self.brightness = value
+        self.led_pwm.duty_u16(value)
+        self.bounce_filter()
+    
+    def clamp(self, value, minimum, maximum):
+        if(value > maximum):
+            value = maximum
+        elif(value < minimum):
+            value = minimum
+            
+        return value
+
+    def bounce_filter(self):
+        if(time.ticks_ms() - self.last_press > 250 and not self.can_press):
+            self.can_press = True
+            print("Can press.")
+    
+knob1 = RotaryKnob(12, 10, 11, 22, 1000) #(PIN_ID, FREQUENCY)
+
+while True:
+    knob1.execute()
